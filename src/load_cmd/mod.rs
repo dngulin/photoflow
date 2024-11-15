62aff3b44f2ee0ef354abaed1cb987aca8ebfdd6
Danil Gulin <d.nk.gulin@gmail.com>
2024-11-15 20:56:24 +0100

    Set window to fullscreen

--- src/load_cmd/mod.rs
+++ src/load_cmd/mod.rs
@@ -12,12 +12,14 @@ pub fn execute<P: AsRef<Path>>(db_path: P) -> anyhow::Result<()> {
     let item_count = db.get_item_count()?;
 
     let app = PhotoFlowApp::new()?;
-    let view_model = Rc::new(ViewModel::new(db));
+    app.window().set_fullscreen(true);
 
+    let view_model = Rc::new(ViewModel::new(db));
     app.set_view_model(view_model.clone().into());
     app.on_set_view_model_range(move |offset, len| {
         view_model.set_range(offset as usize, len as usize)
     });
+
     app.set_item_count(item_count as i32);
 
     app.run()?;
