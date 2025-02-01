1c7f168f10f006f0f61d467c149491ca62df943f
Danil Gulin <d.nk.gulin@gmail.com>
2025-02-01 21:23:20 +0100

    Hash only the file beginning

--- src/indexer/filehash.rs
+++ src/indexer/filehash.rs
@@ -7,22 +7,17 @@ use std::path::Path;
 pub fn calculate<P: AsRef<Path>>(path: P) -> std::io::Result<ArrayString<64>> {
     let input = File::open(path)?;
     let reader = BufReader::new(input);
-    let hash = blake3(reader)?;
+    let hash = blake3_beginning(reader)?;
 
     Ok(hash.to_hex())
 }
 
-fn blake3<R: Read>(mut reader: R) -> std::io::Result<Hash> {
+fn blake3_beginning<R: Read>(mut reader: R) -> std::io::Result<Hash> {
     let mut hasher = blake3::Hasher::new();
-    let mut buffer = [0u8; 1024 * 8];
+    let mut buffer = [0u8; 1024 * 4];
 
-    loop {
-        let count = reader.read(&mut buffer)?;
-        if count == 0 {
-            break;
-        }
-        hasher.update(&buffer[..count]);
-    }
+    let count = reader.read(&mut buffer)?;
+    hasher.update(&buffer[..count]);
 
     Ok(hasher.finalize())
 }
