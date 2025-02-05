57a7053fd1113d2795779250a85474da10aa60e1
Danil Gulin <d.nk.gulin@gmail.com>
2025-02-05 15:59:43 +0100

    Fix RgbImage buffer allocation for HEIC images

--- src/img_decoder.rs
+++ src/img_decoder.rs
@@ -1,6 +1,7 @@
 use crate::exif_orientation::ExifOrientation;
 use image::DynamicImage;
-use libheif_rs::{ColorSpace, HeifContext, LibHeif, RgbChroma};
+use libheif_rs::{ColorSpace, HeifContext, LibHeif, Plane, RgbChroma};
+use std::cmp::Ordering;
 use std::path::Path;
 
 pub fn is_extension_supported(path: &Path) -> bool {
@@ -36,17 +37,32 @@ fn decode_heic(path: &Path) -> anyhow::Result<DynamicImage> {
     let lib_heif = LibHeif::new();
     let heif_image = lib_heif.decode(&handle, ColorSpace::Rgb(RgbChroma::Rgb), None)?;
 
-    let interleaved = heif_image
+    let plane = heif_image
         .planes()
         .interleaved
         .ok_or_else(|| anyhow::anyhow!("Missing interleaved HEIF plane"))?;
 
-    let rgb_image = image::RgbImage::from_raw(
-        interleaved.width,
-        interleaved.height,
-        interleaved.data.to_vec(),
-    )
-    .ok_or_else(|| anyhow::anyhow!("Failed to decode an interleaved HEIF plane"))?;
+    let rgb_buffer = allocate_rgb_buffer(&plane)
+        .ok_or_else(|| anyhow::anyhow!("Failed to allocate RGB buffer"))?;
+
+    let rgb_image = image::RgbImage::from_raw(plane.width, plane.height, rgb_buffer)
+        .ok_or_else(|| anyhow::anyhow!("Invalid RGB buffer"))?;
 
     Ok(DynamicImage::ImageRgb8(rgb_image))
+}
+
+fn allocate_rgb_buffer(p: &Plane<&[u8]>) -> Option<Vec<u8>> {
+    let line_len = (p.width * 3) as usize;
+
+    match p.stride.cmp(&line_len) {
+        Ordering::Less => None,
+        Ordering::Equal => Some(p.data.to_vec()),
+        Ordering::Greater => {
+            let mut buf = Vec::with_capacity(line_len * (p.height as usize));
+            for line in p.data.chunks(p.stride) {
+                buf.extend(&line[..line_len]);
+            }
+            Some(buf)
+        }
+    }
 }
