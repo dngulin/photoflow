06786d0f82a16947603d46ea6242e3862711aa5f
Danil Gulin <d.nk.gulin@gmail.com>
2025-03-07 14:07:21 +0100

    Rename indexer's media loader into preview loader

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -1,5 +1,5 @@
-mod media_loader;
 mod metadata;
+mod preview_loader;
 mod thumbnail;
 
 use crate::db::{IndexDb, InsertionEntry};
@@ -74,7 +74,7 @@ fn collect_paths<P: AsRef<Path>>(source: P, target: &mut HashSet<PathBuf>) {
         .into_iter()
         .filter_map(|r| r.ok())
         .filter(is_not_hidden)
-        .filter(|e| media_loader::is_extension_supported(e.path()))
+        .filter(|e| preview_loader::is_extension_supported(e.path()))
         .map(|e| e.path().to_path_buf());
     target.extend(it);
 }
@@ -136,7 +136,7 @@ fn index_file<P: AsRef<Path>>(
         Some(value) => value,
     };
 
-    let image = media_loader::open(path.as_ref())?;
+    let image = preview_loader::open(path.as_ref())?;
     let thumbnail = image
         .map(|img| thumbnail::squared(&img, 470))
         .oriented(metadata.orientation);
