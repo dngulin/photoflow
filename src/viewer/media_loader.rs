b1dfc521c0b131e2242e78610a91cd35778622ff
Danil Gulin <d.nk.gulin@gmail.com>
2025-03-07 19:18:19 +0100

    Rework media loader and move it into own module

---
+++ src/viewer/media_loader.rs
@@ -0,0 +1,139 @@
+use crate::db::IndexDb;
+use crate::exif_orientation::ExifOrientation;
+use crate::image_loader;
+use crate::video_loader::{Video, VideoLoader};
+use slint::{ComponentHandle, GraphicsAPI, Image, Rgb8Pixel, SharedPixelBuffer, Weak};
+use std::path::Path;
+use std::sync::{Arc, Mutex, MutexGuard};
+
+#[derive(Clone)]
+pub struct MediaLoader {
+    db: Arc<Mutex<IndexDb>>,
+    requested_idx: Arc<Mutex<Option<usize>>>,
+    video_loader: Arc<Mutex<Option<VideoLoader>>>,
+}
+
+pub enum Media {
+    Image(Image),
+    Video(Video),
+}
+
+// Mutex lock wrappers
+impl MediaLoader {
+    fn db(&self) -> MutexGuard<IndexDb> {
+        self.db.lock().unwrap()
+    }
+
+    fn requested_idx(&self) -> MutexGuard<Option<usize>> {
+        self.requested_idx.lock().unwrap()
+    }
+
+    fn video_loader(&self) -> MutexGuard<Option<VideoLoader>> {
+        self.video_loader.lock().unwrap()
+    }
+}
+
+// Public API
+impl MediaLoader {
+    pub fn new(db: Arc<Mutex<IndexDb>>) -> Self {
+        Self {
+            db,
+            requested_idx: Default::default(),
+            video_loader: Default::default(),
+        }
+    }
+
+    pub fn setup_video_loader<F>(&self, api: &GraphicsAPI, request_redraw: F) -> anyhow::Result<()>
+    where
+        F: Fn() + Send + Sync + 'static,
+    {
+        *self.video_loader() = Some(VideoLoader::new(api, request_redraw)?);
+        Ok(())
+    }
+
+    pub fn teardown_video_loader(&self) {
+        *self.video_loader() = None;
+    }
+
+    pub fn cancel_loading(&self) {
+        *self.requested_idx() = None;
+    }
+
+    pub fn load<TApp: ComponentHandle + 'static>(
+        &self,
+        idx: usize,
+        weak_app: Weak<TApp>,
+        on_start: impl FnOnce(TApp, &str),
+        on_finish: impl FnOnce(TApp, anyhow::Result<Media>) + Send + 'static,
+    ) -> anyhow::Result<()> {
+        let mut requested_idx = self.requested_idx();
+
+        if requested_idx.is_some() {
+            return Err(anyhow::anyhow!("Loading is already in progress"));
+        }
+
+        let (path, orientation) = self.db().get_path_and_orientation(idx)?;
+        let app = weak_app
+            .upgrade()
+            .ok_or_else(|| anyhow::anyhow!("Failed to upgrade weak app"))?;
+        on_start(app, &path);
+
+        *requested_idx = Some(idx);
+        rayon::spawn_fifo({
+            let loader = self.clone();
+            move || {
+                let orientation: ExifOrientation = orientation.try_into().unwrap_or_default();
+                let load_result = loader.load_inner(&path, orientation);
+
+                let _ = weak_app.upgrade_in_event_loop(move |app| {
+                    let mut requested_idx = loader.requested_idx();
+                    if *requested_idx != Some(idx) {
+                        return;
+                    }
+                    *requested_idx = None;
+
+                    let load_result = load_result.map(|m| match m {
+                        MediaInner::Image(i) => Media::Image(Image::from_rgb8(i)),
+                        MediaInner::Video(v) => Media::Video(v),
+                    });
+
+                    on_finish(app, load_result);
+                });
+            }
+        });
+
+        Ok(())
+    }
+}
+
+// Media representation that can be sent between threads
+pub enum MediaInner {
+    Image(SharedPixelBuffer<Rgb8Pixel>),
+    Video(Video),
+}
+
+impl MediaLoader {
+    fn load_inner(&self, path: &str, orientation: ExifOrientation) -> anyhow::Result<MediaInner> {
+        let path = Path::new(path);
+
+        if image_loader::is_extension_supported(path) {
+            let image = image_loader::open(path).map(|img| img.oriented(orientation))?;
+            let rgb = image.into_rgb8();
+
+            let buffer = SharedPixelBuffer::<Rgb8Pixel>::clone_from_slice(
+                rgb.as_raw(),
+                rgb.width(),
+                rgb.height(),
+            );
+
+            return Ok(MediaInner::Image(buffer));
+        }
+
+        let mut video_loader = self.video_loader();
+        let video_loader = video_loader
+            .as_mut()
+            .ok_or_else(|| anyhow::anyhow!("Video loader is not initialized"))?;
+
+        video_loader.load(path).map(MediaInner::Video)
+    }
+}
--- src/viewer/mod.rs
+++ src/viewer/mod.rs
@@ -1,16 +1,13 @@
 mod image_grid_model;
-
-use self::image_grid_model::ImageGridModel;
+mod media_loader;
 
 use crate::db::IndexDb;
-use crate::exif_orientation::ExifOrientation;
-use crate::image_loader;
 use crate::ui::{MediaViewerBridge, MediaViewerModel, PhotoFlowApp, ViewerState};
-use crate::video_loader::VideoLoader;
+use crate::video_loader::Video;
+use crate::viewer::image_grid_model::ImageGridModel;
+use crate::viewer::media_loader::{Media, MediaLoader};
 use anyhow::anyhow;
-use slint::{
-    ComponentHandle, Image, RenderingState, Rgb8Pixel, SharedPixelBuffer, SharedString, Weak,
-};
+use slint::{ComponentHandle, Image, RenderingState, Weak};
 use std::path::Path;
 use std::rc::Rc;
 use std::sync::{Arc, Mutex, MutexGuard};
@@ -38,121 +35,141 @@ pub fn bind_models(app: &PhotoFlowApp, db: Arc<Mutex<IndexDb>>) -> anyhow::Resul
     Ok(())
 }
 
-#[derive(Clone)]
-struct MediaLoader {
-    db: Arc<Mutex<IndexDb>>,
-    requested_idx: Arc<Mutex<Option<usize>>>,
-    player: Arc<Mutex<Option<VideoLoader>>>,
-}
+#[derive(Clone, Default)]
+struct PlayingVideo(Arc<Mutex<Option<Video>>>);
 
-impl MediaLoader {
-    pub fn new(db: Arc<Mutex<IndexDb>>) -> Self {
-        Self {
-            db,
-            requested_idx: Default::default(),
-            player: Default::default(),
-        }
+impl PlayingVideo {
+    fn inner(&self) -> MutexGuard<Option<Video>> {
+        self.0.lock().unwrap()
     }
 
-    pub fn ensure_requested(&self, idx: usize) -> Option<()> {
-        let target = self.requested_idx.lock().ok()?;
-
-        if target.eq(&Some(idx)) {
-            Some(())
-        } else {
-            None
-        }
+    pub fn set(&self, video: Video) {
+        *self.inner() = Some(video);
     }
 
-    pub fn player(&self) -> MutexGuard<Option<VideoLoader>> {
-        self.player.lock().unwrap()
+    pub fn curr_video_gl_frame(&self) -> Option<Image> {
+        self.inner().as_ref().and_then(|p| p.current_frame_gl_ref())
+    }
+
+    pub fn copy_current_frame_and_stop(&self) -> Option<Image> {
+        let mut inner = self.inner();
+        if let Some(video) = inner.as_ref() {
+            let opt_frame = video.current_frame_copy();
+            *inner = None;
+            return opt_frame;
+        }
+
+        None
+    }
+
+    pub fn stop(&self) {
+        *self.inner() = None;
     }
 }
 
 pub fn bind_media_loader(app: &PhotoFlowApp, db: Arc<Mutex<IndexDb>>) {
     let bridge = app.global::<MediaViewerBridge>();
     let loader = MediaLoader::new(db);
+    let playing = PlayingVideo::default();
 
-    {
-        let weak_app = app.as_weak();
-        let loader = loader.clone();
-        bridge.on_load(move |idx| {
-            let _ = load(weak_app.clone(), &loader, idx as usize);
-        });
-    }
-
-    {
-        let weak_app = app.as_weak();
-        let loader = loader.clone();
-        bridge.on_clear(move || {
-            let _ = clear(weak_app.clone(), &loader);
-        });
-    }
-
-    {
-        let loader = loader.clone();
+    bridge.on_load({
         let app_weak = app.as_weak();
-        let _ = app
-            .window()
-            .set_rendering_notifier(move |state, api| match state {
+        let loader = loader.clone();
+        let playing = playing.clone();
+
+        move |idx| {
+            let _ = load(&app_weak, &loader, idx as usize, &playing);
+        }
+    });
+
+    bridge.on_clear({
+        let app_weak = app.as_weak();
+        let loader = loader.clone();
+        let playing = playing.clone();
+
+        move || {
+            let _ = clear(&app_weak, &loader, &playing);
+        }
+    });
+
+    app.window()
+        .set_rendering_notifier({
+            let loader = loader.clone();
+            let playing = playing.clone();
+            let app_weak = app.as_weak();
+
+            move |state, api| match state {
                 RenderingState::RenderingSetup => {
-                    let app_weak = app_weak.clone();
-                    let request_redraw = move || {
-                        let _ = app_weak.upgrade_in_event_loop(|app| {
-                            app.window().request_redraw();
-                        });
-                    };
-                    *loader.player() = VideoLoader::new(api, request_redraw).ok();
+                    let request_redraw = get_req_redraw_callback(&app_weak);
+                    let _ = loader.setup_video_loader(api, request_redraw);
                 }
                 RenderingState::BeforeRendering => {
-                    if let Some(frame) = loader
-                        .player()
-                        .as_ref()
-                        .and_then(|p| p.playback())
-                        .and_then(|p| p.current_frame_gl_ref())
-                    {
-                        let app = app_weak.unwrap();
-                        let bridge = app.global::<MediaViewerBridge>();
-                        bridge.set_model(MediaViewerModel {
-                            image: frame,
-                            ..bridge.get_model()
-                        });
+                    if let Some(frame) = playing.curr_video_gl_frame() {
+                        try_set_bridge_image(&app_weak, frame);
                     }
                 }
                 RenderingState::RenderingTeardown => {
-                    loader.player().take();
+                    loader.teardown_video_loader();
                 }
                 _ => {}
-            });
+            }
+        })
+        .ok();
+}
+
+fn get_req_redraw_callback(app_weak: &Weak<PhotoFlowApp>) -> impl Fn() + Send + Sync + 'static {
+    let weak_app = app_weak.clone();
+    move || {
+        let _ = weak_app.upgrade_in_event_loop(|app| {
+            app.window().request_redraw();
+        });
     }
 }
 
-fn load(weak_app: Weak<PhotoFlowApp>, loader: &MediaLoader, idx: usize) -> Option<()> {
-    let mut requested_idx = loader.requested_idx.lock().ok()?;
-
-    if *requested_idx == Some(idx) {
-        return None;
+fn try_set_bridge_image(app_weak: &Weak<PhotoFlowApp>, image: Image) {
+    if let Some(app) = app_weak.upgrade() {
+        let bridge = app.global::<MediaViewerBridge>();
+        let model = bridge.get_model();
+        bridge.set_model(MediaViewerModel { image, ..model });
     }
+}
 
-    let app = weak_app.upgrade()?;
-    let (path, orientation) = {
-        let db = loader.db.lock().ok()?;
-        db.get_path_and_orientation(idx).ok()?
-    };
-    let orientation: ExifOrientation = orientation.try_into().unwrap_or_default();
-    let file_name = Path::new(&path).file_name()?.to_str()?;
+fn load(
+    weak_app: &Weak<PhotoFlowApp>,
+    loader: &MediaLoader,
+    idx: usize,
+    playing: &PlayingVideo,
+) -> anyhow::Result<()> {
+    loader.load(
+        idx,
+        weak_app.clone(),
+        {
+            let playing_video = playing.clone();
+            move |app, path| {
+                on_load_start(app, path, playing_video);
+            }
+        },
+        {
+            let playing_video = playing.clone();
+            move |app, result| {
+                on_load_finish(app, playing_video, result);
+            }
+        },
+    )
+}
+
+fn on_load_start(app: PhotoFlowApp, path: &str, playing: PlayingVideo) {
+    let file_name = Path::new(path)
+        .file_name()
+        .and_then(|n| n.to_str())
+        .unwrap_or(path);
 
     let bridge = app.global::<MediaViewerBridge>();
     let model = bridge.get_model();
     let mut image = model.image;
 
-    if let Some(player) = loader.player.lock().unwrap().as_mut() {
-        if let Some(playback) = player.playback() {
-            if let Some(frame) = playback.current_frame_copy() {
-                image = frame;
-            }
-            player.unload()
-        }
+    if let Some(frame) = playing.copy_current_frame_and_stop() {
+        image = frame;
     }
 
     bridge.set_model(MediaViewerModel {
@@ -160,103 +177,47 @@ fn load(weak_app: Weak<PhotoFlowApp>, loader: &MediaLoader, idx: usize) -> Optio
         file_name: file_name.into(),
         image,
     });
+}
 
-    let weak_app = weak_app.clone();
-    let loader = loader.clone();
+fn on_load_finish(app: PhotoFlowApp, playing: PlayingVideo, result: anyhow::Result<Media>) {
+    let bridge = app.global::<MediaViewerBridge>();
+    let model = bridge.get_model();
 
-    *requested_idx = Some(idx);
-    rayon::spawn_fifo(move || {
-        let opt_media = load_media(&loader, idx, path, orientation);
-
-        let _ = weak_app.upgrade_in_event_loop(move |app| {
-            let mut requested = loader.requested_idx.lock().unwrap();
-            if *requested != Some(idx) {
-                return;
-            }
-            *requested = None;
-
-            match opt_media {
-                None => set_failed_to_load_media(&app),
-                Some(media) => match media {
-                    Media::Image(buffer) => set_media_loaded(&app, Some(buffer)),
-                    Media::Video => set_media_loaded(&app, None),
+    match result {
+        Ok(media) => {
+            bridge.set_model(MediaViewerModel {
+                state: ViewerState::Loaded,
+                file_name: model.file_name,
+                image: match media {
+                    Media::Image(img) => img,
+                    Media::Video(video) => {
+                        playing.set(video);
+                        model.image
+                    }
                 },
-            }
-        });
-    });
-
-    Some(())
+            });
+        }
+        Err(_) => {
+            bridge.set_model(MediaViewerModel {
+                state: ViewerState::FailedToLoad,
+                file_name: model.file_name,
+                image: Image::default(),
+            });
+        }
+    };
 }
 
-enum Media {
-    Image(SharedPixelBuffer<Rgb8Pixel>),
-    Video,
-}
-
-fn load_media(
+fn clear(
+    weak_app: &Weak<PhotoFlowApp>,
     loader: &MediaLoader,
-    idx: usize,
-    path: String,
-    orientation: ExifOrientation,
-) -> Option<Media> {
-    loader.ensure_requested(idx)?;
-
-    let path = Path::new(&path);
-
-    if image_loader::is_extension_supported(path) {
-        let image = image_loader::open(Path::new(&path))
-            .map(|img| img.oriented(orientation))
-            .ok()?;
-        let rgb = image.into_rgb8();
-        let buffer = SharedPixelBuffer::<Rgb8Pixel>::clone_from_slice(
-            rgb.as_raw(),
-            rgb.width(),
-            rgb.height(),
-        );
-        return Some(Media::Image(buffer));
-    }
-
-    loader
-        .player()
-        .as_mut()
-        .and_then(|player| player.load(path).ok())
-        .map(|_| Media::Video)
-}
-
-fn set_media_loaded(app: &PhotoFlowApp, buffer: Option<SharedPixelBuffer<Rgb8Pixel>>) {
-    let bridge = app.global::<MediaViewerBridge>();
-    let model = bridge.get_model();
-    bridge.set_model(MediaViewerModel {
-        state: ViewerState::Loaded,
-        file_name: model.file_name,
-        image: buffer.map(Image::from_rgb8).unwrap_or(model.image),
-    });
-}
-
-fn set_failed_to_load_media(app: &PhotoFlowApp) {
-    let bridge = app.global::<MediaViewerBridge>();
-    let model = bridge.get_model();
-    bridge.set_model(MediaViewerModel {
-        state: ViewerState::FailedToLoad,
-        file_name: model.file_name,
-        image: Image::default(),
-    });
-}
-
-fn clear(weak_app: Weak<PhotoFlowApp>, loader: &MediaLoader) -> Option<()> {
-    *loader.requested_idx.lock().ok()? = None;
-
-    if let Some(player) = loader.player().as_mut() {
-        player.unload()
-    }
+    playing: &PlayingVideo,
+) -> Option<()> {
+    loader.cancel_loading();
+    playing.stop();
 
     let app = weak_app.upgrade()?;
     let bridge = app.global::<MediaViewerBridge>();
-    bridge.set_model(MediaViewerModel {
-        state: ViewerState::Loaded,
-        file_name: SharedString::default(),
-        image: Image::default(),
-    });
+    bridge.set_model(MediaViewerModel::default());
 
     Some(())
 }
