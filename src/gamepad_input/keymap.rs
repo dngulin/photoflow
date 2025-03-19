ccf6465421f5b961ca842a5e8c9df3d4bed3b895
Danil Gulin <d.nk.gulin@gmail.com>
2025-03-19 20:22:50 +0100

    Add keymap for triggers

--- src/gamepad_input/keymap.rs
+++ src/gamepad_input/keymap.rs
@@ -5,6 +5,10 @@ pub fn btn_to_key(value: Button) -> Option<Key> {
     match value {
         Button::South => Some(Key::Return),
         Button::East => Some(Key::Escape),
+        Button::LeftTrigger => Some(Key::PageUp),
+        Button::LeftTrigger2 => Some(Key::Home),
+        Button::RightTrigger => Some(Key::PageDown),
+        Button::RightTrigger2 => Some(Key::End),
         Button::DPadUp => Some(Key::UpArrow),
         Button::DPadDown => Some(Key::DownArrow),
         Button::DPadLeft => Some(Key::LeftArrow),
